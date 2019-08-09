d7cd4ed88059ac3a5c6c990927d0b632e9507760
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-09 16:25:08 -0500

    Modelos

---
+++ src/app/models/tenni.ts
@@ -0,0 +1,24 @@
+export class Tenni {
+    // public nombre: string;
+    // public marca: string;
+    // public color: string;
+    // public precio: number;
+    // public stock: boolean;
+
+    // constructor(nombre: string, marca: string, color: string, precio: number, stock: boolean) {
+    //     this.nombre = nombre;
+    //     this.marca = marca;
+    //     this.color = color;
+    //     this.precio = precio;
+    //     this.stock = stock;
+    // }
+
+    // este ultimo constructor hace la misma funcion que el codigo de arriba
+    constructor(
+        public nombre: string,
+        public marca: string,
+        public color: string,
+        public precio: number,
+        public stock: boolean
+    ) {}
+}
--- src/app/tennis/tennis.component.ts
+++ src/app/tennis/tennis.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {Tenni} from '../models/tenni';
 
 @Component({
     selector: 'tennis',
     templateUrl: './tennis.component.html'
 })
-export class TennisComponent {
+export class TennisComponent implements OnInit {
+
     public titulo = 'Componente de tennis';
+    public tennis: Array<Tenni>;
+
+    constructor() {
+        this.tennis = [
+            new Tenni('Rebook classic', 'Rebook', 'Blancos', 1300.00, true),
+            new Tenni('Nike Air Force One', 'Nike', 'Negors', 1500.00, true),
+            new Tenni('Nike Runner', 'Nike', 'Rojos', 1200.00, true)
+        ];
+    }
+
+    ngOnInit(): void {
+        console.log(this.tennis);
+    }
 }
