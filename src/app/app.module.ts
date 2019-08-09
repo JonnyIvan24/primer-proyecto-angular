e57293f331a86341d936609e6c226f11aa3f7782
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-09 14:34:14 -0500

    multiples componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 import {VideojuegoComponent} from './videojuego/videoJuego.component';
+import {TennisComponent} from './tennis/tennis.component';
 
 // este decoador nos ayuda a configurar el modulo
 @NgModule({
   // en declarations cargamos los componenetes
   declarations: [
     AppComponent,
-    VideojuegoComponent
+    VideojuegoComponent,
+    TennisComponent
   ],
   // cargamos modulos de angular o externos
   imports: [
---
+++ src/app/tennis/tennis.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'tennis',
+    templateUrl: './tennis.component.html'
+})
+export class TennisComponent {
+    public titulo = 'Componente de tennis';
+}
