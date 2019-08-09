efe8faf0832fb245564f3aa6ff80ba1a00919028
Jonathan Perez (Johnny) <jonny.perez.u@gmail.com>
2019-08-09 15:29:11 -0500

    importar  y exportar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import {configuracion} from './models/configuracion';
+
 @Component({
   // nombre de la dierctiva o etiqueta html
   selector: 'app-root',
@@ -10,7 +12,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public title = 'aprendiendo Angular';
+  public descripcion: string;
+  public config;
+
   public mostrar: boolean = true;
+
+  constructor() {
+    this.title = configuracion.titulo;
+    this.descripcion = configuracion.descripcion;
+    this.config = configuracion;
+  }
 
   ocultarVideojuegos(value: boolean) {
     this.mostrar = value;
---
+++ src/app/models/configuracion.ts
@@ -0,0 +1,6 @@
+export let configuracion = {
+    color:          'red',
+    fondo:          'blue',
+    titulo:         'Aplicación en Angular',
+    descripcion:    'Aplicación para aprender angular'
+};
